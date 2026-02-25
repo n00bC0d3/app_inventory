114131eede09d7e7e334d6149ec84bf453555030
n00bC0d30bc0d3 <kurniawan.moonlight@gmail.com>
2026-02-25 11:39:03 +0700

    fix psql connection test file

--- testing.sh
+++ testing.sh
@@ -5,7 +5,11 @@ sudo journalctl -u inventory_app -f
 sudo tail -f /var/log/nginx/access.log
 
 # Test Redis connection
+sudo apt-get update
+sudo apt-get install redis-server
 redis-cli -h 10.20.10.13 -a Password123* ping
   
 # Test DB connection
-psql -h 10.20.10.2:5432 -U admin -d postgres
+sudo apt update
+sudo apt install postgresql-client -y
+psql -h 10.20.10.2 -p 5432 -U admin -d postgres
